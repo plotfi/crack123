3e5cd29c0b06db9078befeff8998002a5385dc53
Puyan Lotfi <puyan@puyan.org>
2020-02-09 20:20:03 -0800

    first commit

---
+++ crack.cpp
@@ -0,0 +1,40 @@
+
+// ** Behavioral Prep Grid:
+//
+// Question          Project1    Project2    Project3
+// Challenges
+// Mistake/Fails
+// Enjoyed
+// Leadership
+// Conflicts
+// What to do Diff
+
+// ** What are your weaknesses?
+
+// ** Big O
+// * Recursive functions are often O(branches^depth)
+// * fully balanced tree is O(2^logN)
+
+// Use proof by induction..
+// *
+// **
+// ***
+// ****
+//
+// *****  ****
+// ******  ***
+// *******  **
+// ********  *
+// *********
+//
+// (10 * 9) /2
+//
+// O((N * (N-1))/2)
+
+#include <cstdio>
+
+
+int main() {
+  printf("hello\n");
+}
+

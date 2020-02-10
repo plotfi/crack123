1e032545aa0a73962cbac765d341f33ae866fa74
Puyan Lotfi <puyan@puyan.org>
2020-02-09 23:59:45 -0800

    Notes on technical Qs

--- crack.cpp
+++ crack.cpp
@@ -30,6 +30,17 @@
 // (10 * 9) /2
 //
 // O((N * (N-1))/2)
+//
+// TODO: Resume Big O at Page 52
+
+// >>> Page 60: Technical Questions
+//
+// Core Data Structures: Linked List, Trees, Tries, Graphs, Stacks, Queues,
+//                       Heaps, Vectors, Hash Tables.
+// TODO: Study up LLVM ADTs
+// Algos: BFS, DFS, Binary Search, Merge Sort, Quick Sort, Bucket Sort
+// Cocepts: Bit Manip, Memory (stack vs heap), Recursion, Dynamic Programming,
+//          Big O Time and Space
 
 #include <cstdio>
 
